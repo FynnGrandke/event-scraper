180d15564bcf95ab69c317030ca28483698785b9
Fynn Grandke <fynn.grandke@web.de>
2019-10-19 09:16:57 +0900

    Created entry point to run modules

--- webscraper.py
+++ webscraper.py
@@ -1,15 +1,5 @@
-import requests
-import urllib.request
-import time
-from bs4 import BeautifulSoup
+from event_modules.meetup_module import meetup
 
-url = 'https://www.meetup.com/find/events/?allMeetups=true&radius=25&userFreeform=Tokyo%2C+Japan&mcId=c1023444&mcName=Tokyo%2C+JP&month=10&day=19&year=2019'
-
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-something = soup.findAll('span', {'itemprop': 'name'})
-
-for tag in soup.findAll('span', {'itemprop': 'name'}):
-  print(tag.string)
+if __name__ == "__main__":
+    meetup()
+    print('END')
