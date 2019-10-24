a6a5fb5dbf714618a0cacde3aa8f7e728afa4078
Fynn Grandke <fynn.grandke@web.de>
2019-10-24 19:15:37 +0900

    Added and removed comment and improved print comment

--- event_modules/meetup_module.py
+++ event_modules/meetup_module.py
@@ -3,14 +3,14 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
+
 def meetup():
-  url = 'https://www.meetup.com/find/events/?allMeetups=true&radius=25&userFreeform=Tokyo%2C+Japan&mcId=c1023444&mcName=Tokyo%2C+JP&month=10&day=19&year=2019'
+    # FIXME: @FynnGrandke URL needs to be adjustable
+    url = 'https://www.meetup.com/find/events/?allMeetups=true&radius=25&userFreeform=Tokyo%2C+Japan&mcId=c1023444&mcName=Tokyo%2C+JP&month=10&day=19&year=2019'
 
-  response = requests.get(url)
+    response = requests.get(url)
 
-  soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-  # something = soup.findAll('span', {'itemprop': 'name'})
-
-  for tag in soup.findAll('span', {'itemprop': 'name'}):
-    print(tag.string)
+    for tag in soup.findAll('span', {'itemprop': 'name'}):
+        print('Meetup: ' + tag.string)
