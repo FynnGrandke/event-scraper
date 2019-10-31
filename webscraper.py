40b3895babbff263df5f0e00760af2b4068f8e64
Fynn Grandke <fynn.grandke@web.de>
2019-10-31 22:49:19 +0900

    Updated the entry file

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,6 @@
-from event_modules.meetup_module import meetup
+from event_modules.meetup_module import getMeetupEvents
+from event_modules.eventbrite_module import getEventbriteEvents
 
 if __name__ == "__main__":
-    meetup()
+    getEventbriteEvents()
     print('END')
