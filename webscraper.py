c385bede6eb91c5f7e2ef316f06cb032b1ec97f4
Fynn Grandke <fynn.grandke@web.de>
2019-11-01 23:26:31 +0900

    Updated main file for testing

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 from event_modules.meetup_module import getMeetupEvents
 from event_modules.eventbrite_module import getEventbriteEvents
+from event_modules.alleventsin_module import getAlleventsinEvents
 
 if __name__ == "__main__":
-    getEventbriteEvents()
+    getAlleventsinEvents()
     print('END')
