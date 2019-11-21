22146190d19c944019f9b2eb20c64e41df8252af
Fynn Grandke <fynn.grandke@web.de>
2019-11-21 22:30:57 +0900

    Added arguments parser to search for specific events

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,25 @@
 from event_modules.meetup_module import getMeetupEvents
 from event_modules.eventbrite_module import getEventbriteEvents
 from event_modules.alleventsin_module import getAlleventsinEvents
+import argparse
+
 
 if __name__ == "__main__":
-    getAlleventsinEvents()
-    print('END')
+    parser = argparse.ArgumentParser("event scraper")
+    parser.add_argument(
+        "website", help="The website from which to get events.", type=str)
+    args = parser.parse_args()
+
+    # getAlleventsinEvents()
+
+    if args.website == 'meetup':
+        getMeetupEvents()
+    else:
+        if args.website == 'eventbrite':
+            getEventbriteEvents()
+        else:
+            if args.website == 'alleventsin':
+                getAlleventsinEvents()
+            else:
+                print('NO EVENTS FOR', args.website)
+    print('END for', args.website)
